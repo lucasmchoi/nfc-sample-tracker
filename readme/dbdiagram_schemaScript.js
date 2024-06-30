7831b8e049db7c4530476a4ffb11cc8103c100b4
Luca Choi <73029406+lucasmchoi@users.noreply.github.com>
2024-06-30 15:21:56 +0200

    :memo: modify database ERM

--- readme/dbdiagram_schemaScript.js
+++ readme/dbdiagram_schemaScript.js
@@ -1,49 +1,3 @@
-db.createCollection("new-plates", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      title: "new-plates",
-      required: ["_id"],
-      properties: {
-        "_id": { bsonType: "objectId" },
-        "plate_id": { bsonType: "objectId" },
-        "creation-date": { bsonType: "date" },
-      },
-    },
-  },
-});
-
-db.createCollection("new-samples", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      title: "new-samples",
-      required: ["_id"],
-      properties: {
-        "_id": { bsonType: "objectId" },
-        "sample_id": { bsonType: "objectId" },
-        "creation-date": { bsonType: "date" },
-      },
-    },
-  },
-});
-
-db.createCollection("new-users", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      title: "new-users",
-      required: ["_id"],
-      properties: {
-        "_id": { bsonType: "objectId" },
-        "user_id": { bsonType: "objectId" },
-        "uuid": { bsonType: "string" },
-        "creation-date": { bsonType: "date" },
-      },
-    },
-  },
-});
-
 db.createCollection("users", {
   validator: {
     $jsonSchema: {
@@ -55,23 +9,6 @@ db.createCollection("users", {
         "name": { bsonType: "object", title: "name", properties: { "first": { bsonType: "string" }, "last": { bsonType: "string" }, }, },
         "email": { bsonType: "string" },
         "userid": { bsonType: "string" },
-        "creation-date": { bsonType: "date" },
-        "modification-date": { bsonType: "date" },
-      },
-    },
-  },
-});
-
-db.createCollection("locations", {
-  validator: {
-    $jsonSchema: {
-      bsonType: "object",
-      title: "locations",
-      required: ["_id"],
-      properties: {
-        "_id": { bsonType: "objectId" },
-        "name": { bsonType: "string" },
-        "location": { bsonType: "object", title: "location", properties: { "address": { bsonType: "object", title: "address", properties: { "street": { bsonType: "string" }, "zip": { bsonType: "string" }, "city": { bsonType: "string" }, "country": { bsonType: "string" }, }, }, "room": { bsonType: "string" }, }, },
         "creation-date": { bsonType: "date" },
         "modification-date": { bsonType: "date" },
       },
@@ -107,7 +44,7 @@ db.createCollection("samples", {
         "_id": { bsonType: "objectId" },
         "sample-number": { bsonType: "int" },
         "owners": { bsonType: "array", items: { bsonType: "object" } },
-        "information": { bsonType: "object", title: "information", properties: { "material": { bsonType: "string" }, "orientation": { bsonType: "string" }, "doping": { bsonType: "string" }, "growth": { bsonType: "string" }, "note": { bsonType: "string" }, "damaged": { bsonType: "object", title: "damaged", properties: { "date": { bsonType: "date" }, "note": { bsonType: "string" }, }, }, "lost": { bsonType: "object", title: "lost", properties: { "date": { bsonType: "date" }, "note": { bsonType: "string" }, }, }, }, },
+        "information": { bsonType: "object", title: "information", properties: { "origin": { bsonType: "objectId" }, "material": { bsonType: "string" }, "orientation": { bsonType: "string" }, "doping": { bsonType: "string" }, "growth": { bsonType: "string" }, "note": { bsonType: "string" }, "damaged": { bsonType: "object", title: "damaged", properties: { "date": { bsonType: "date" }, "note": { bsonType: "string" }, }, }, "lost": { bsonType: "object", title: "lost", properties: { "date": { bsonType: "date" }, "note": { bsonType: "string" }, }, }, }, },
         "locations": { bsonType: "array", items: { bsonType: "object" } },
         "images": { bsonType: "array", items: { bsonType: "objectId" } },
         "files": { bsonType: "array", items: { bsonType: "objectId" } },
@@ -149,6 +86,74 @@ db.createCollection("fs.files", {
         "contentType": { bsonType: "string" },
         "aliases": { bsonType: "array", items: { bsonType: "string" } },
         "metadata": { bsonType: "any" },
+      },
+    },
+  },
+});
+
+db.createCollection("new-plates", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      title: "new-plates",
+      required: ["_id"],
+      properties: {
+        "_id": { bsonType: "objectId" },
+        "plate_id": { bsonType: "objectId" },
+        "plate-number": { bsonType: "int" },
+        "creation-date": { bsonType: "date" },
+      },
+    },
+  },
+});
+
+db.createCollection("new-users", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      title: "new-users",
+      required: ["_id"],
+      properties: {
+        "_id": { bsonType: "objectId" },
+        "user_id": { bsonType: "objectId" },
+        "uuid": { bsonType: "string" },
+        "creation-date": { bsonType: "date" },
+      },
+    },
+  },
+});
+
+db.createCollection("new-samples", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      title: "new-samples",
+      required: ["_id"],
+      properties: {
+        "_id": { bsonType: "objectId" },
+        "sample_id": { bsonType: "objectId" },
+        "sample-number": { bsonType: "int" },
+        "material": { bsonType: "string" },
+        "orientation": { bsonType: "string" },
+        "origin": { bsonType: "string" },
+        "creation-date": { bsonType: "date" },
+      },
+    },
+  },
+});
+
+db.createCollection("locations", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      title: "locations",
+      required: ["_id"],
+      properties: {
+        "_id": { bsonType: "objectId" },
+        "name": { bsonType: "string" },
+        "location": { bsonType: "object", title: "location", properties: { "address": { bsonType: "object", title: "address", properties: { "street": { bsonType: "string" }, "zip": { bsonType: "string" }, "city": { bsonType: "string" }, "country": { bsonType: "string" }, }, }, "room": { bsonType: "string" }, }, },
+        "creation-date": { bsonType: "date" },
+        "modification-date": { bsonType: "date" },
       },
     },
   },
